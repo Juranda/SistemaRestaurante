18879cdaeb9fb3bdb6c4633847552e4c25436f2a
Felipe <felipeonlinebr@gmail.com>
2026-06-11 22:07:51 -0300

    StatusPedido agora fica em ItemPedido facilitando a organização na hora de prepara produtos 1 por 1. MesaId alterado para NumeroMesa. ReadOnlyList nos ItemsPedido concentrar o controle da lista para a class do Pedido. Fluxo de alterar quantidade dos items.

--- SistemaRestaurante.Domain/Entidades/ItemPedido.cs
+++ SistemaRestaurante.Domain/Entidades/ItemPedido.cs
@@ -4,18 +4,21 @@ namespace SistemaRestaurante.Domain.Entidades;
 
 public class ItemPedido
 {
-    public int Quantidade { get; private set; }
+    public int PedidoId { get; private set; }
     public int ProdutoId { get; private set; }
+    public int Quantidade { get; private set; }
+    public StatusPedido Status { get; private set; }
 
-    private ItemPedido(int quantidade, int produtoId)
+    private ItemPedido(int pedidoId, int quantidade, int produtoId)
     {
+        PedidoId = pedidoId;
         Quantidade = quantidade;
         ProdutoId = produtoId;
     }
 
-    public static Result<ItemPedido> Criar(int quantidade, int produtoId)
+    public static Result<ItemPedido> Criar(int pedidoId, int quantidade, int produtoId)
     {
-        var item = new ItemPedido(quantidade, produtoId);
+        var item = new ItemPedido(pedidoId, quantidade, produtoId);
         var result = item.Validar();
 
         if (result.IsError)
@@ -29,8 +32,40 @@ public class ItemPedido
     public Result Validar()
     {
         return Result.All(
+            Validacoes.ValidarNumero(nameof(PedidoId), PedidoId, 1, int.MaxValue),
             Validacoes.ValidarNumero(nameof(Quantidade), Quantidade, 1, int.MaxValue),
             Validacoes.ValidarNumero(nameof(ProdutoId), ProdutoId, 1, int.MaxValue)
         );
+    }
+
+    public Result AtualizarStatus()
+    {
+        switch (Status)
+        {
+            case StatusPedido.EM_PREPARO:
+                Status = StatusPedido.PRONTO;
+                return Result.Success();
+            case StatusPedido.PRONTO:
+                Status = StatusPedido.ENTREGUE;
+                return Result.Success();
+            case StatusPedido.ENTREGUE:
+                return ErrosPedido.AlteraStatusPedidoFinalizado();
+            default:
+                throw new Exception("Status do item inválido");
+        }
+    }
+
+    public Result AlterarQuantidade(int quantidade)
+    {
+        var item = new ItemPedido(PedidoId, quantidade, ProdutoId);
+        var result = item.Validar();
+
+        if(result.IsError)
+        {
+            return result;
+        }
+
+        Quantidade = quantidade;
+        return Result.Success();
     }
 }
--- SistemaRestaurante.Domain/Entidades/Pedido.cs
+++ SistemaRestaurante.Domain/Entidades/Pedido.cs
@@ -6,53 +6,58 @@ public class Pedido
 {
     public int Id { get; private set; }
     public string NomeCliente { get; private set; }
-    private const int MIN_CARACTERES_NOME = 5;
-    private const int MAX_CARACTERES_NOME = 50;
-    public int MesaId { get; private set; }
-    public StatusPedido Status { get; private set; }
-    public List<ItemPedido> ItemsPedido { get; private set; }
+    public const int MIN_CARACTERES_NOME = 5;
+    public const int MAX_CARACTERES_NOME = 50;
+    public int NumeroMesa { get; private set; }
+    public IReadOnlyList<ItemPedido> ItemsPedido => itemsPedido.AsReadOnly();
+    private List<ItemPedido> itemsPedido { get; set; }
 
-    public bool Finalizado => Status == StatusPedido.ENTREGUE;
+    public bool Pronto => itemsPedido.All(x => x.Status == StatusPedido.PRONTO);
 
-    private Pedido(int id, string nomeCliente, int mesaId, List<ItemPedido> itemsPedido)
+    private Pedido(int id, string nomeCliente, int numeroMesa, List<ItemPedido> itemsPedido)
     {
         Id = id;
         NomeCliente = nomeCliente;
-        MesaId = mesaId;
-        ItemsPedido = itemsPedido;
+        NumeroMesa = numeroMesa;
+        this.itemsPedido = itemsPedido;
     }
 
-    public static Result<Pedido> Criar(int id, string nomeCliente, int mesaId, List<ItemPedido> itemsPedido)
+    public StatusPedido Status()
     {
-        var result = Result.All(
-            Validacoes.ValidarNumero(nameof(id), id, 1, int.MaxValue),
-            Validacoes.ValidarTexto(nameof(nomeCliente), nomeCliente, MIN_CARACTERES_NOME, MAX_CARACTERES_NOME),
-            itemsPedido.Select(x => x.Validar()).Aggregate((prev, curr) => prev.CombineResult(curr))
-        );
+        if(itemsPedido.Count == 0)
+        {
+            return StatusPedido.ENTREGUE;
+        }
+
+        if (itemsPedido.All(x => x.Status == StatusPedido.EM_PREPARO))
+        {
+            return StatusPedido.EM_PREPARO;
+        }
+
+        if (itemsPedido.All(x => x.Status == StatusPedido.PRONTO))
+        {
+            return StatusPedido.PRONTO;
+        }
+
+        if (itemsPedido.All(x => x.Status == StatusPedido.ENTREGUE))
+        {
+            return StatusPedido.ENTREGUE;
+        }
+
+        throw new Exception("Status inválido");
+    }
+
+    public static Result<Pedido> Criar(int id, string nomeCliente, int numeroMesa, List<ItemPedido> itemsPedido)
+    {
+        var pedido = new Pedido(id, nomeCliente, numeroMesa, itemsPedido);
+        var result = pedido.Validar();
 
         if (result.IsError)
         {
             return (Result<Pedido>)result;
         }
 
-        return new Pedido(id, nomeCliente, mesaId, itemsPedido);
-    }
-
-    public Result AtualizarStatus()
-    {
-        switch (Status)
-        {
-            case StatusPedido.EM_PREPARO:
-                Status = StatusPedido.PRONTO;
-                return Result.Success();
-            case StatusPedido.PRONTO:
-                Status = StatusPedido.ENTREGUE;
-                return Result.Success();
-            case StatusPedido.ENTREGUE:
-                return PedidoErrors.AlteraStatusPedidoFinalizado();
-            default:
-                throw new Exception("Status do pedido inválido");
-        }
+        return pedido;
     }
 
     public Result Validar()
@@ -60,12 +65,24 @@ public class Pedido
         return Result.All(
             Validacoes.ValidarNumero(nameof(Id), Id, 1, int.MaxValue),
             Validacoes.ValidarTexto(nameof(NomeCliente), NomeCliente, MIN_CARACTERES_NOME, MAX_CARACTERES_NOME),
-            Validacoes.ValidarNumero(nameof(MesaId), MesaId, 1, int.MaxValue)
+            Validacoes.ValidarNumero(nameof(NumeroMesa), NumeroMesa, 1, int.MaxValue)
         );
     }
-}
 
-public static class PedidoErrors
-{
-    public static Result AlteraStatusPedidoFinalizado() => new Error("PED-DOM-001", "O pedido já foi finalizado. O status não pode ser alterado.", ErrorTypes.Conflict);
+    public Result AlterarQuantidade(int produtoId, int quantidade)
+    {
+        var item = itemsPedido.FirstOrDefault(x => x.ProdutoId == produtoId);
+
+        if(item is null)
+        {
+            return ErrosPedido.AlterarQuantidadeDeItemInexistente();
+        }
+
+        if(quantidade == 0)
+        {
+            itemsPedido.Remove(item);
+        }
+
+        return item.AlterarQuantidade(quantidade);
+    }
 }
